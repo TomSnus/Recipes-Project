3ca8ef59b8644ba2b9cb4a427c2353cd037ccf0e
Tom <senteswins@gmail.com>
2021-01-11 14:59:31 +0100

    init

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,9 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-@Output() selectedTarget = new EventEmitter<string>();
 
-  onSelect(target: string){
-    this.selectedTarget.emit(target);
-  }
 }
