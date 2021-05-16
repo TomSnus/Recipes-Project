403c46af7eea30aefb51d804ba0a169902c79167
Tom <senteswins@gmail.com>
2021-05-16 14:51:44 +0200

    firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebaseApiKey: 'AIzaSyDJrbt9LW8L2AttJp_JSP2yKBJKf4JKe5I'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseApiKey: 'AIzaSyDJrbt9LW8L2AttJp_JSP2yKBJKf4JKe5I'
 };
 
 /*
